239e6670b59d9a5472014ab8ba94cb33f2deb28a
codesandbox-test <watoru374215@gmail.com>
2023-04-05 08:03:53 +0000

    セクション6完了

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
+import "./styles.css";
+import { Router } from "./router/Router";
 
 export default function App() {
-  return (
-    <div className="App">
-      <PrimaryButton>テスト</PrimaryButton>
-      <SecondaryButton>検索</SecondaryButton>
-      <br />
-      <SearchInput />
-    </div>
-  );
+  return <Router />;
 }
