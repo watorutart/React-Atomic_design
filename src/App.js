8c2b97bcab7884aa66dcac184ae780850fdfff7d
codesandbox-test <watoru374215@gmail.com>
2023-04-05 06:40:30 +0000

    6-31.Moleculeの作成

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import { PrimaryButton } from "./components/atom/button/PrimaryButton";
-import { SecondaryButton } from "./components/atom/button/SecondaryButton";
+import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molecules/SearchInput";
 
 export default function App() {
   return (
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
+      <br />
+      <SearchInput />
     </div>
   );
 }
